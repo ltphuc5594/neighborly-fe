326bf1e7f3838650cc8e1f7043bcdd248eedbd30
Le Thanh Phuc <ltphuc5594@gmail.com>
2023-10-15 23:58:22 +0700

    Init project

---
+++ settings.py
@@ -0,0 +1,14 @@
+#--------- Flask settings
+SERVER_HOST = '0.0.0.0' # Update this for the appropriate front-end website when up
+SERVER_PORT = 5000
+FLASK_DEBUG = True # Do not use debug mode in prod
+
+# Flask-Restplus settings
+SWAGGER_UI_DOC_EXPANSION = 'list'
+RESTPLUS_VALIDATE = True
+RESTPLUS_MASK_SWAGGER = False
+RESTPLUS_404_HELP = True
+API_VERSION = 'v1'
+
+#-------- Azure constants
+API_URL = "https://neighborly2023.azurewebsites.net/api/"
